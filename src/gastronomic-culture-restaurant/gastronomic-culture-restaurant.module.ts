47fb2acb65d04d9eead10bd6dc4922386ae92f1d
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2024-08-31 14:51:27 -0500

    fix: added missing imports on modules

--- src/gastronomic-culture-restaurant/gastronomic-culture-restaurant.module.ts
+++ src/gastronomic-culture-restaurant/gastronomic-culture-restaurant.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GastronomicCultureRestaurantService } from './gastronomic-culture-restaurant.service';
+import { RestaurantEntity } from '../restaurant/restaurant.entity';
+import { GastronomicCultureEntity } from '../gastronomic-culture/gastronomic-culture.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GastronomicCultureService } from '../gastronomic-culture/gastronomic-culture.service';
+import { RestaurantService } from 'src/restaurant/restaurant.service';
 
 @Module({
-  providers: [GastronomicCultureRestaurantService]
+  providers: [
+    GastronomicCultureRestaurantService,
+    GastronomicCultureService,
+    RestaurantService,
+  ],
+  imports: [
+    TypeOrmModule.forFeature([GastronomicCultureEntity, RestaurantEntity]),
+  ],
 })
 export class GastronomicCultureRestaurantModule {}
--- src/shared/testing-utils/typeorm-testing-config.ts
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -2,6 +2,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantEntity } from '../../restaurant/restaurant.entity';
 import { GastronomicCultureEntity } from '../../gastronomic-culture/gastronomic-culture.entity';
 import { CountryEntity } from '../../country/country.entity';
+import { RecipeEntity } from '../../recipe/recipe.entity';
 
 export const TypeOrmTestingConfig = () => [
   TypeOrmModule.forRoot({
@@ -16,6 +17,7 @@ export const TypeOrmTestingConfig = () => [
     GastronomicCultureEntity,
     RestaurantEntity,
     CountryEntity,
+    RecipeEntity,
     // TODO: Add missing entities @fedemelo @pdazad @dburgos26
   ]),
 ];
