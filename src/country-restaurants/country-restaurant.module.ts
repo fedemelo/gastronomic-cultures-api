5082746541fb9ed35491a704005f0ea23eaf5f7f
fedemelo <f.melo@uniandes.edu.co>
2024-09-01 20:00:44 -0500

    Remove controllers and dtos, unnecessary in current deliverable

--- src/country-restaurants/country-restaurant.module.ts
+++ src/country-restaurants/country-restaurant.module.ts
@@ -3,11 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantEntity } from '../restaurant/restaurant.entity';
 import { CountryEntity } from '../country/country.entity';
 import { CountryRestaurantService } from './country-restaurant.service';
-import { CountryRestaurantController } from './country-restaurant.controller';
 
 @Module({
   providers: [CountryRestaurantService],
   imports: [TypeOrmModule.forFeature([CountryEntity, RestaurantEntity])],
-  controllers: [CountryRestaurantController],
 })
 export class CountryRestaurantModule {}
--- src/country/country.dto.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { IsNotEmpty, IsString } from 'class-validator';
-export class CountryDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly name: string;
-}
