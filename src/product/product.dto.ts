9051a6c2c470f8e43d266c939ec02c4394053117
fedemelo <f.melo@uniandes.edu.co>
2024-09-15 21:42:30 -0500

    Remove unused import in Product DTO

--- src/product/product.dto.ts
+++ src/product/product.dto.ts
@@ -1,21 +1,19 @@
-import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ProductDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
 
-    @IsString()
-    @IsNotEmpty()
-    readonly name: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly description: string;
 
-    @IsString()
-    @IsNotEmpty()
-    readonly description: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly history: string;
 
-    @IsString()
-    @IsNotEmpty()
-    readonly history: string;
-
-    @IsString()
-    @IsNotEmpty()
-    readonly category: string;
-
+  @IsString()
+  @IsNotEmpty()
+  readonly category: string;
 }
